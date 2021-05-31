3d2f2948cfe3cf76b841e5a1aad84171471b42b5
Saksham Awasthi <74664860+Cipher-08@users.noreply.github.com>
2021-05-31 23:17:37 +0530

    Create Floyd's-triangle.cpp

---
+++ Floyd's-triangle.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main(){
+   //
+/*Code here Saksham*/
+    //
+    int number;
+    cin>>number;
+    int count=1;
+    for (int i = 0; i < number; i++)
+    {
+        /* code */
+        for (int j = 0; j < i; j++)
+        {
+            /* code */
+            cout<<count;
+            count++;
+
+        }
+        cout<<endl;
+        
+    }
+    
+}
+
+
+// input:: :
+//  10
+// output:: :
+//       1
+//       23
+//       456
+//       78910
+//       1112131415
+//       161718192021
+//       22232425262728 
+//       2930313233343536 
+//       373839404142434445
+
