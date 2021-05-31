44b40bf0b3d75247e8037805f4306fcc97b86b64
Saksham Awasthi <74664860+Cipher-08@users.noreply.github.com>
2021-05-31 23:06:41 +0530

    Create half_pyramid_of_numbers.cpp

---
+++ half_pyramid_of_numbers.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main(){
+   //
+/*Code here Saksham*/
+    //
+
+    int number;
+    cin>>number;
+    for (int i = 1; i <= number; i++)
+    {
+        /* code */
+        for (int j = 1; j <= i; j++)
+        {
+            /* code */
+            cout<<i;
+        }
+        cout<<endl;
+        
+    }
+    
+}
