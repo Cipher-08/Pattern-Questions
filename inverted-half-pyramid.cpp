f3dbae29b66c0a1d79f58fc36fd5dfd13af72877
Saksham Awasthi <74664860+Cipher-08@users.noreply.github.com>
2021-05-31 23:05:24 +0530

    Create inverted-half-pyramid.cpp

---
+++ inverted-half-pyramid.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main(){
+   //
+/*Code here Saksham*/
+    //
+    int columns;
+    cin>>columns;
+
+    for (int i = columns ; i >=0; i--)
+    {
+        /* code */
+        for (int j = 0; j < i; j++)
+        {
+            /* code */
+            cout<<"*";
+        }
+        cout << endl;
+
+    }
+    
+}
