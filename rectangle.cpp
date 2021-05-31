6ab8455f4cac30efa4f5549dd83b531be4090953
Saksham Awasthi <74664860+Cipher-08@users.noreply.github.com>
2021-05-31 23:03:18 +0530

    Create rectangle.cpp
    *******
    *******

---
+++ rectangle.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+   //
+/*Code here Saksham*/
+    //
+    for(int i=0;i<5;i++)//number of rows
+    {
+        for(int j=0;j<6;j++)//number of columns
+        {
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+}
